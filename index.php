fcd11203a7031c54520ba6a72729f92dc153f094
Mariaericka <98100913+Mariaericka@users.noreply.github.com>
2022-11-07 16:53:05 +0800

    navbar
    changes in  navbar

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php include('partials-front/navbar.php'); ?>
+
 <!DOCTYPE html>
 <html lang="en"></html>
 
@@ -33,28 +35,7 @@
     
 <!-- Header section starts here -->
 
-<header>
 
-    <a href="index.php" class="logo"><img src="images/omg-logo.png" image style="object-fit: contain; width: 70px;"></a>
-
-    <nav class="navbar">
-        <a href="#Home">HOME</a>
-        <a href="ordersection\ALL.php">ORDER</a>
-        <a href="blog2.html">BLOG</a>
-        <a href="franchising.html">FRANCHISE</a>
-        <a href="career.html">CAREER</a>
-        <a href="loc.html">LOCATION</a>
-        <a href="contact-us.html">CONTACT US</a>
-    </nav>
-
-    <div class="icons">
-        <i class="fas fa-bars" id="menu-bars"></i>
-        <i class="fas fa-search" id="search-icon"></i>
-        <i class="fa-brands fa-facebook-f" id="facebook"></i>
-        <i class="fa-solid fa-cart-shopping"></i>
-        <i class="fa-solid fa-user"></i>        
-    </div>
-</header>
 
 <!-- Header section ends here -->
 
---
+++ partials-front/navbar.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   
+   <link rel="stylesheet" href="/omgph/css/indexx.css">
+<header>
+
+    <a href="index.php" class="logo"><img src="images/omg-logo.png" image style="object-fit: contain; width: 70px;"></a>
+
+    <nav class="navbar">
+        <a href="#Home">HOME</a>
+        <a href="ordersection\ALL.php">MENU</a>
+        <a href="blog2.html">BLOG</a>
+        <a href="franchising.html">FRANCHISE</a>
+        <a href="career.html">CAREER</a>
+        <a href="loc.html">LOCATION</a>
+        <a href="contact-us.html">CONTACT US</a>
+    </nav>
+
+    <div class="icons">
+        <i class="fas fa-bars" id="menu-bars"></i>
+        <i class="fas fa-search" id="search-icon"></i>
+        <i class="fa-brands fa-facebook-f" id="facebook"></i>
+        <i class="fa-solid fa-cart-shopping"></i>
+        <i class="fa-solid fa-user"></i>        
+    </div>
+</header>
