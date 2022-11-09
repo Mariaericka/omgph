bffe9e29a7d0f71161b168708888aca28818c5e5
Mariaericka <98100913+Mariaericka@users.noreply.github.com>
2022-11-09 23:03:31 +0800

    making of login page
    log in on progress

---
+++ JS/login.js
@@ -0,0 +1,33 @@
+
+   $(function() {
+    $(".btn1").click(function() {
+       $(".form-signin").toggleClass("form-signin-left");
+     $(".form-signup").toggleClass("form-signup-left");
+     $(".frame").toggleClass("frame-long");
+     $(".signup-inactive").toggleClass("signup-active");
+     $(".signin-active").toggleClass("signin-inactive");
+     $(".forgot").toggleClass("forgot-left");   
+     $(this).removeClass("idle").addClass("active");
+    });
+ });
+ 
+ $(function() {
+    $(".btn1-signup").click(function() {
+   $(".nav1").toggleClass("nav1-up");
+   $(".form-signup-left").toggleClass("form-signup-down");
+   $(".success").toggleClass("success-left"); 
+   $(".frame").toggleClass("frame-short");
+    });
+ });
+ 
+ $(function() {
+    $(".btn1-signin").click(function() {
+   $(".btn1-animate").toggleClass("btn1-animate-grow");
+   $(".welcome").toggleClass("welcome-left");
+   $(".cover-photo").toggleClass("cover-photo-down");
+   $(".frame").toggleClass("frame-short");
+   $(".profile-photo").toggleClass("profile-photo-down");
+   $(".btn1-goback").toggleClass("btn-goback-up");
+   $(".forgot").toggleClass("forgot-fade");
+    });
+ });
