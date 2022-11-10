6d39fddfcbba0e569d349c0bc9e574a68eaa2ca7
Mariaericka <98100913+Mariaericka@users.noreply.github.com>
2022-11-10 23:52:59 +0800

    testing new connection

--- partials-front/navbar.php
+++ partials-front/navbar.php
@@ -1,5 +1,4 @@
-<?php include('../config/constants.php'); ?>
-
+<?php include('C:\xampp\htdocs\omgph\config\connect.php'); ?>
 
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
@@ -21,11 +20,15 @@
     </nav>
    
     <div class="icons">
-   
+    <?php
+            $count_cart_items = $conn->prepare("SELECT * FROM `tbl_cart` WHERE user_id = ?");
+            $count_cart_items->execute([$user_id]);
+            $total_cart_items = $count_cart_items->rowCount();
+         ?>
         <i class="fas fa-bars" id="menu-bars"></i>
         <i class="fas fa-search" id="search-icon"></i>
         <i class="fa-brands fa-facebook-f" id="facebook"></i>
-        <i class="fa-solid fa-cart-shopping"></i>
+        <i class="fa-solid fa-cart-shopping"></i><span>(<?= $total_cart_items; ?>)</span>
         <div class="dropdown">
        <button class="user"><i class="fa-solid fa-user" id="user"></i></button>
         <div class="content">  
