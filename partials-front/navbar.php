649778cbc36fa6bea233cf10c730812734ef8678
Mariaericka <98100913+Mariaericka@users.noreply.github.com>
2022-11-10 23:22:59 +0800

    Update navbar.php

--- partials-front/navbar.php
+++ partials-front/navbar.php
@@ -1,3 +1,7 @@
+<?php include('../config/constants.php'); ?>
+
+
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 <link rel="stylesheet" href="/omgph/css/indexx.css">
 
@@ -15,8 +19,9 @@
         <a href="loc.html">LOCATION</a>
         <a href="contact-us.html">CONTACT US</a>
     </nav>
-
+   
     <div class="icons">
+   
         <i class="fas fa-bars" id="menu-bars"></i>
         <i class="fas fa-search" id="search-icon"></i>
         <i class="fa-brands fa-facebook-f" id="facebook"></i>
@@ -32,3 +37,5 @@
  
     
 </header>
+
+ 
